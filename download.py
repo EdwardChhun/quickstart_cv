67084c6901ccbc2168e4de348b23fab45fa12db5
Edward Chhun <edwardchhun3@gmail.com>
2024-11-15 15:16:22 -0800

    removed contents

--- download.py
+++
@@ -1,6 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="r7qSIBlL0ryPm5qonSPH")
-project = rf.workspace("school-95f9t").project("human-detection-uerkn")
-version = project.version(3)
-dataset = version.download("yolov11")
-                
